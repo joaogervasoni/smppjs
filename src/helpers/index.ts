17d814879bc511b5e292e843c92d6fa4d3e09286
Zaetic <joaogabrielggs@gmail.com>
2024-04-27 14:57:17 -0300

    feat: Add optionalparams helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,1 +1,2 @@
 export * from './commandsId';
+export * from './optionalParams';
---
+++ src/helpers/optionalParams.ts
@@ -0,0 +1,10 @@
+/**
+ * The SMPP supported Optional Parameters and their associated Tag
+ *
+ * ref: Documentation SMPP_v3_4
+ */
+const optionalParams = {
+    receipted_message_id: 0x001e,
+};
+
+export { optionalParams };

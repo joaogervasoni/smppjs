b2c7cdffc28fd707bfc1da0871337de114dbe4f3
Zaetic <joaogabrielggs@gmail.com>
2024-04-27 16:21:26 -0300

    feat: Create pdu and initBuffer

---
+++ src/PDU.ts
@@ -0,0 +1,16 @@
+import { commandsId } from './helpers';
+
+export default class PDU {
+    call(command: 'bind_transceiver', sequenceNumber: number) {}
+
+    initPduBuffer({ command_length, commandId, sequenceNumber }: { command_length: number; commandId: 'bind_transceiver'; sequenceNumber: number }): Buffer {
+        let pduBuffer = Buffer.alloc(command_length);
+
+        pduBuffer.writeUInt32BE(command_length, 0);
+        pduBuffer.writeUInt32BE(commandsId[commandId], 4);
+        // pduBuffer.writeUInt32BE(commandStatus, 8);
+        pduBuffer.writeUInt32BE(sequenceNumber, 12);
+
+        return pduBuffer;
+    }
+}

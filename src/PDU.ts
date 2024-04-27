2adb10c07bdfc28488a429614e60afe3fbdec124
Zaetic <joaogabrielggs@gmail.com>
2024-04-27 16:23:24 -0300

    feat: Add commandStatus to buffer

--- src/PDU.ts
+++ src/PDU.ts
@@ -5,10 +5,11 @@ export default class PDU {
 
     initPduBuffer({ command_length, commandId, sequenceNumber }: { command_length: number; commandId: 'bind_transceiver'; sequenceNumber: number }): Buffer {
         let pduBuffer = Buffer.alloc(command_length);
+        const commandStatus = 0;
 
         pduBuffer.writeUInt32BE(command_length, 0);
         pduBuffer.writeUInt32BE(commandsId[commandId], 4);
-        // pduBuffer.writeUInt32BE(commandStatus, 8);
+        pduBuffer.writeUInt32BE(commandStatus, 8);
         pduBuffer.writeUInt32BE(sequenceNumber, 12);
 
         return pduBuffer;

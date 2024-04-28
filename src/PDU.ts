4a773e6461bf2286cbbd53f0118de80599afe9fa
Joao Gervasoni <joaogabrielggs@gmail.com>
2024-04-28 17:57:54 -0300

    feat: Calc buffer length and call function

--- src/PDU.ts
+++ src/PDU.ts
@@ -1,31 +1,39 @@
-import { commandsId } from './helpers';
+import { Socket } from 'net';
+import { commandsId, commandsParams } from './helpers';
 import { octets } from './octets';
 
 export default class PDU {
-    call(command: 'bind_transceiver', sequenceNumber: number) {}
+    call(command: 'bind_transceiver', sequenceNumber: number, socket: Socket) {
+        const commandId = commandsId[command];
+        const commandParams = commandsParams[commandId];
 
-    initPduBuffer({
-        command_length,
-        commandName,
-        sequenceNumber,
-    }: {
-        command_length: number;
-        commandName: 'bind_transceiver';
-        sequenceNumber: number;
-    }): Buffer {
-        let pduBuffer = Buffer.alloc(command_length);
+        let commandLength = 16;
+
+        const paramEntries = Object.entries(commandParams);
+        for (let index = 0; index < paramEntries.length; index++) {
+            const element = paramEntries[index][1];
+            commandLength += octets[element.type].size(element.value);
+        }
+
+        let buffer = this.initPduBuffer({ commandLength, commandId, sequenceNumber });
+        buffer = this.writePduBuffer({ pduBuffer: buffer, pduParams: commandParams, offset: 16 });
+        socket.write(buffer);
+    }
+
+    private initPduBuffer({ commandLength, commandId, sequenceNumber }: { commandLength: number; commandId: number; sequenceNumber: number }): Buffer {
+        let pduBuffer = Buffer.alloc(commandLength);
         const commandStatus = 0;
 
         // header
-        pduBuffer.writeUInt32BE(command_length, 0);
-        pduBuffer.writeUInt32BE(commandsId[commandName], 4);
+        pduBuffer.writeUInt32BE(commandLength, 0);
+        pduBuffer.writeUInt32BE(commandId, 4);
         pduBuffer.writeUInt32BE(commandStatus, 8);
         pduBuffer.writeUInt32BE(sequenceNumber, 12);
 
         return pduBuffer;
     }
 
-    writePduBuffer({
+    private writePduBuffer({
         pduParams,
         pduBuffer,
         offset,

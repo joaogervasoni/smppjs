275c1407633f5250cd8bcaab3a30cc850fce9ec5
Joao Gervasoni <joaogabrielggs@gmail.com>
2024-05-04 13:35:50 -0300

    feat: Create validation to string length

--- src/dtos/bind_transceiver.ts
+++ src/dtos/bind_transceiver.ts
@@ -1,5 +1,12 @@
 import { DTO, DTOFunction } from '../types';
 
+const MAX_LENGTH = {
+    system_id: 16,
+    password: 9,
+    system_type: 13,
+    address_range: 41,
+};
+
 export interface BindTransceiver extends DTO {
     system_id: { type: 'Cstring'; value: string };
     password: { type: 'Cstring'; value: string };
@@ -14,8 +21,14 @@ export interface BindTransceiverFunction extends DTOFunction<{ systemIdValue: st
     ({ systemIdValue, passwordValue }: { systemIdValue: string; passwordValue: string }): BindTransceiver;
 }
 
-export const bindTransceiverDTO: BindTransceiverFunction = ({ systemIdValue, passwordValue }: { systemIdValue: string; passwordValue: string }): BindTransceiver => {
-    return {
+export const bindTransceiverDTO: BindTransceiverFunction = ({
+    systemIdValue,
+    passwordValue,
+}: {
+    systemIdValue: string;
+    passwordValue: string;
+}): BindTransceiver => {
+    const dto: BindTransceiver = {
         system_id: { type: 'Cstring', value: systemIdValue },
         password: { type: 'Cstring', value: passwordValue },
         system_type: { type: 'Cstring', value: '' },
@@ -24,6 +37,22 @@ export const bindTransceiverDTO: BindTransceiverFunction = ({ systemIdValue, pas
         addr_npi: { type: 'Int8', value: 0 },
         address_range: { type: 'Cstring', value: '' },
     };
+
+    validateDto(dto);
+    return dto;
 };
 
-// validateDto()
+/**
+ * Add length plus one to add 00 validation
+ * 
+ * Ref: Page 49 - SMPP_v5
+ * @param dto BindTransceiver
+ */
+const validateDto = (dto: BindTransceiver): void => {
+    const validator = Object.entries(MAX_LENGTH);
+    for (let index = 0; index < validator.length; index += 1) {
+        if (dto[validator[index][0]].value.toString().length + 1 > validator[index][1]) {
+            throw new Error(`${validator[index][0]} need to be minor than ${validator[index][1]}`);
+        }
+    }
+};

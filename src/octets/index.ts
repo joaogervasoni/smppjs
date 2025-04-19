1137a9fd70f4942af564e8d828fb54bdaef78cff
Joao Gervasoni <joaogabrielggs@gmail.com>
2025-04-19 11:52:15 -0300

    feat: Add int16 octet

--- src/octets/index.ts
+++ src/octets/index.ts
@@ -1,7 +1,9 @@
-import { Cstring } from "./cstring";
-import { Int8 } from "./int8";
+import { Cstring } from './cstring';
+import { Int8 } from './int8';
+import { Int16 } from './int16';
 
 export const octets = {
     Cstring,
-    Int8
-}
+    Int8,
+    Int16,
+};
---
+++ src/octets/int16.ts
@@ -0,0 +1,23 @@
+/**
+ * Octet size 2
+ */
+class Int16 {
+    /**
+     * Write a int16 in a buffer
+     */
+    static write({ buffer, value, offset }: { buffer: Buffer; value: number; offset: number }): Buffer {
+        let newBuffer = buffer;
+        newBuffer.writeUInt16BE(value || 0, offset);
+
+        return newBuffer;
+    }
+
+    /**
+     * Return size of int16
+     */
+    static size() {
+        return 2;
+    }
+}
+
+export { Int16 };

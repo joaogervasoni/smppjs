bd6c286e08c85fe525121269f2ef5fce9e5d0b79
Joao Gervasoni <joaogabrielggs@gmail.com>
2025-04-19 11:53:00 -0300

    feat: Add message payload optional

--- src/constains/optionalParams.ts
+++ src/constains/optionalParams.ts
@@ -3,8 +3,9 @@
  *
  * ref: Documentation SMPP_v3_4
  */
-const optionalParams = {
+const optionalParams: Record<string, number> = {
     receipted_message_id: 0x001e,
+    message_payload: 0x0424,
 };
 
 export { optionalParams };

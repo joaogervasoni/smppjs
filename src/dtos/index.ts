83e889f2b46470aed2299937dcbe87d83f537d9a
Joao Gervasoni <joaogabrielggs@gmail.com>
2025-06-19 17:29:26 -0300

    feat: Add receiver to dto call

--- src/dtos/index.ts
+++ src/dtos/index.ts
@@ -5,6 +5,8 @@ import { enquireLinkRespDTO } from './enquire_link_resp';
 import { enquireLinkDTO } from './enquire_link';
 import { submitSmDTO } from './submit_sm';
 import { submitSmRespDTO } from './submit_sm_resp';
+import { bindReceiverDTO } from './bind_receiver';
+import { bindReceiverRespDTO } from './bind_receiver_resp';
 
 const DTOs: Record<string, DTOFunction<never, DTO>> = {
     bind_transceiver: bindTransceiverDTO,
@@ -13,6 +15,8 @@ const DTOs: Record<string, DTOFunction<never, DTO>> = {
     submit_sm_resp: submitSmRespDTO,
     enquire_link: enquireLinkDTO,
     enquire_link_resp: enquireLinkRespDTO,
+    bind_receiver: bindReceiverDTO,
+    bind_receiver_resp: bindReceiverRespDTO,
 };
 
 const getDTO = <T extends DTOFunction | BindTransceiverFunction | BindTransceiverRespFunction | SubmitSmFunction | EnquireLinkFunction>(name: string): T => {

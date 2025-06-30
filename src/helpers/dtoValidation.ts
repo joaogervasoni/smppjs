3de6cbae56aa6bc35a00bbbe7df32d4661924f5f
Joao Gervasoni <joaogabrielggs@gmail.com>
2025-06-29 22:01:07 -0300

    refactor: Change validation dto

--- src/helpers/dtoValidation.ts
+++ src/helpers/dtoValidation.ts
@@ -8,29 +8,43 @@ import { DTO, DateType } from '../types';
  * Ref: Page 49 - SMPP_v5
  * @param dto DTO
  */
-export const dtoValidation = ({ dto, MAX_LENGTH, DATE_TYPE }: { dto: DTO; MAX_LENGTH: Record<string, number>; DATE_TYPE: Record<string, DateType> }): void => {
+export const dtoValidation = ({
+    dto,
+    MAX_LENGTH,
+    DATE_TYPE,
+}: {
+    dto: DTO;
+    MAX_LENGTH?: Record<string, number>;
+    DATE_TYPE?: Record<string, DateType>;
+}): void => {
     const dtoRecord: Record<string, { type: string; value: string | number | Buffer }> = dto.command;
-    const validator = Object.entries(MAX_LENGTH);
-    const dateValidator = Object.entries(DATE_TYPE);
 
-    for (let index = 0; index < validator.length; index += 1) {
-        const fieldName = validator[index][0];
+    if (MAX_LENGTH) {
+        const validator = Object.entries(MAX_LENGTH);
 
-        if (dtoRecord[fieldName].value && dtoRecord[fieldName].value.toString().length + 1 > validator[index][1]) {
-            throw new Error(`${validator[index][0]} need to be minor than ${validator[index][1]}`);
+        for (let index = 0; index < validator.length; index += 1) {
+            const fieldName = validator[index][0];
+
+            if (dtoRecord[fieldName].value && dtoRecord[fieldName].value.toString().length + 1 > validator[index][1]) {
+                throw new Error(`${validator[index][0]} need to be minor than ${validator[index][1]}`);
+            }
         }
     }
 
-    for (let index = 0; index < dateValidator.length; index += 1) {
-        const fieldName = dateValidator[index][0];
-        const fieldType = dateValidator[index][1];
+    if (DATE_TYPE) {
+        const dateValidator = Object.entries(DATE_TYPE);
 
-        if (dtoRecord[fieldName].value) {
-            const value = dtoRecord[fieldName].value.toString() as string;
+        for (let index = 0; index < dateValidator.length; index += 1) {
+            const fieldName = dateValidator[index][0];
+            const fieldType = dateValidator[index][1];
 
-            if (value) {
-                if (value.endsWith('R', value.length - 1) && fieldType === DateType.ABSOLUTE) {
-                    throw new Error(`${validator[index][0]} need to be relative ${validator[index][1]}`);
+            if (dtoRecord[fieldName].value) {
+                const value = dtoRecord[fieldName].value.toString() as string;
+
+                if (value) {
+                    if (value.endsWith('R', value.length - 1) && fieldType === DateType.ABSOLUTE) {
+                        throw new Error(`${dateValidator[index][0]} need to be relative ${dateValidator[index][1]}`);
+                    }
                 }
             }
         }

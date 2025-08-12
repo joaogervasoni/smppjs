fe466560b67e47e646071e6fa2dc9118e7ae3f6c
Joao Gervasoni <34172553+joaogervasoni@users.noreply.github.com>
2025-08-12 12:57:08 -0300

    refactor: Only stringify objects

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -6,10 +6,10 @@ class Logger {
         private readonly options: {
             debug: boolean;
         },
-    ) {}
+    ) { }
 
     debug(message: string, object?: object) {
-        const messageEmit = `${message} - ${JSON.stringify(object ? object : {})}`;
+        const messageEmit = `${message} - ${object ? JSON.stringify(object) : {}}`;
 
         if (this.options.debug) {
             this.socket.emit('debug', messageEmit);

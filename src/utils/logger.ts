40c32484edf5e9429a95d3a362616da82b558a8e
Joao Gervasoni <34172553+joaogervasoni@users.noreply.github.com>
2025-08-12 22:50:18 -0300

    fix: Add text obj to log message

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -6,10 +6,10 @@ class Logger {
         private readonly options: {
             debug: boolean;
         },
-    ) { }
+    ) {}
 
     debug(message: string, object?: object) {
-        const messageEmit = `${message} - ${object ? JSON.stringify(object) : {}}`;
+        const messageEmit = `${message} - ${object ? JSON.stringify(object) : '{}'}`;
 
         if (this.options.debug) {
             this.socket.emit('debug', messageEmit);
